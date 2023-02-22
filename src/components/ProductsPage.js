9bd80629bce1e9abf06e66d01e21e3c787e57ffe
Zefanja Timorason <z.timorason@gmail.com>
2023-02-22 23:00:10 +0100

    second commit

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -1,16 +1,41 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import SearchBar from "./SearchBar";
 import jsonData from './../data.json'
 import ProductTable from "./ProductTable";
 
 
-function ProductsPage() {
-    const [products, setProducts] = useState(jsonData)
 
+function ProductsPage(props) {
+    const [products, setProducts] = useState(jsonData)
+    const [products2, setProducts2] = useState(jsonData)
+    
+    function filterProduct(element) {
+    //    const spullen = [...products] 
+   
+    const filteredItems = products2.filter((ele) => {
+        return ele.name.toLowerCase().includes(element.toLowerCase())
+    })
+        setProducts2(filteredItems)
+    }
+  
+  
+//     function searchResult(element) {
+       
+   
+        
+//         const spullen= products.filter((item2) => {
+//             return item2.name.toLocaleLowerCase().includes(element.toLocaleLowerCase())
+//         })
+//         setSpullen(spullen)
+// }
+
+   
     return (
         <div>
             <h1>IronStore</h1>
-            <SearchBar />
+ 
+            <SearchBar filterProduct={filterProduct}/>
+        
             <ProductTable itemList={products}/>
         </div>
     )
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,46 @@
 import React from "react"
+import { useState } from "react"
+import jsonData from './../data.json'
+// return item.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase)
 
-function SearchBar() {
 
+function SearchBar(props, {data}) {
+    const [inputValue, setInputvalue] = useState('')
+
+    let inputValueHandler = (e) => {
+        let inputted = e.target.value.toLowerCase();
+        setInputvalue(inputted)
+       
+    }
+
+  
+   
+    // const [searchTerm, setSearchTerm] = useState('')
+    
+
+    // function searchResult2(event) {
+    //     setSearchTerm(event.target.value)
+     
+        
+
+// const searchResulted = event => {
+//     setSearchTerm(event.target.value)
+//     props.searchResult(event.target.value)
+    
+// }
+    
     return (
         <div>
             <h3> Searchbar goes here</h3>
+    <input type='text' onChange={ inputValueHandler} value={inputValue} />
+            {/* <ul>
+            {filteredItems.map((item) => (
+                <li key={item.id}>{item.name}</li>
+            ))}
+            </ul> */}
+           
        </div>
-   )
-}
+)}
+
 
 export default SearchBar
